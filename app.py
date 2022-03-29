e68dc1de1595ff623fb8f7a14e7e67024c9e6f5e
omri yakubovsky <omriya@cloudzone.io>
2022-03-29 19:29:30 +0300

    new

---
+++ app.py
@@ -0,0 +1,14 @@
+#!flask/bin/python
+from flask import Flask, request, request_started
+app = Flask(__name__)
+counter = 0
+@app.route('/', methods=["POST", "GET"])
+def post():
+    global counter
+    if request.method == "POST":
+        counter+=1
+        return "Hmm, Plus 1 please "
+    else:
+        return str(f"The counter is: {counter} ")
+if __name__ == '__main__':
+    app.run(debug=True,threaded=True,host='0.0.0.0',port=443)
